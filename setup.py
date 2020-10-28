3c109d1ececa5f617b1e8f00f41939107181db68
Philip Evans <philip.fevans@gmail.com>
2020-10-28 20:15:13 +1100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,12 @@ setup(
     author="phil",
     author_email="",
     description="",
+    install_requires=[
+        "pandas",
+        "jira",
+        "donfig",
+        "loguru",
+        "sqlalchemy",
+        "psycopg2-binary",
+    ],
 )
